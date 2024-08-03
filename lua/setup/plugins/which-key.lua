f6fa381fd8070205bca958439f3564dcd1a01be4
VIISAUS <8653971+ouyang-jing@user.noreply.gitee.com>
2024-08-04 01:40:39 +0800

    [fix] which-key error

--- lua/setup/plugins/which-key.lua
+++ lua/setup/plugins/which-key.lua
@@ -8,6 +8,8 @@ return {
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        triggers = {
+            { "<leader>", mode = { "n", "v" } },
+        }, -- refer to the configuration section below
     },
 }
