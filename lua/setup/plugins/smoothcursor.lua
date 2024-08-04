1159a507f49204a244045fbb639ade20a108be9d
VIISAUS <8653971+ouyang-jing@user.noreply.gitee.com>
2024-08-04 16:11:21 +0800

    [fix] rm smooth-cursor

--- lua/setup/plugins/smoothcursor.lua
+++
@@ -1,8 +0,0 @@
-return{ 
-  'gen740/SmoothCursor.nvim',
-  config = function()
-  require("smoothcursor").setup({
-    type = "matrix",           -- Cursor movement calculation method, choose "default", "exp" (exponential) or "matrix".
-    })
-  end
-}
